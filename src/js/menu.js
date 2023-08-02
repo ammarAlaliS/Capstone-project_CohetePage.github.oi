073c0dc0f4f1163e6d310352c5b886d92aaad989
ammaralialis <ammaralialiwa@gmail.com>
2023-08-02 16:51:00 -0600

    finish project. resolve linter error

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,17 +1,22 @@
 const hamburgerButtonImg = document.querySelector('.hamburgerButtonImg');
 const closeIcon = document.querySelector('.fa-solid');
+const links = document.querySelectorAll('.menuLinks');
 
 function openMenu() {
-  const menu = document.querySelector('.menu');
+  const menu = document.querySelector('.menuTwo');
   menu.style.display = 'block';
   document.body.style.overflow = 'hidden';
 }
 
 function closeMenu() {
-  const menu = document.querySelector('.menu');
+  const menu = document.querySelector('.menuTwo');
   menu.style.display = 'none';
   document.body.style.overflow = 'auto';
 }
 
 hamburgerButtonImg.addEventListener('click', openMenu);
 closeIcon.addEventListener('click', closeMenu);
+
+links.forEach((link) => {
+  link.addEventListener('click', closeMenu);
+});
